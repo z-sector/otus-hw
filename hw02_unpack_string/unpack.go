38a330365fe0c30c6527195fa37b2a55707ece53
George Ramaniuk <george.ramaniuk@gmail.com>
2022-12-11 12:25:19 +0300

    Add hw02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,55 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
+
+const EscapeSymbol = '\\'
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	if s == "" {
+		return "", nil
+	}
+
+	var (
+		builder  strings.Builder
+		prev     string
+		isEscape bool
+	)
+
+	for _, char := range s {
+		if isEscape && !(unicode.IsDigit(char) || char == EscapeSymbol) {
+			return "", ErrInvalidString
+		}
+
+		if char == EscapeSymbol && !isEscape {
+			isEscape = true
+			continue
+		}
+
+		if unicode.IsDigit(char) && !isEscape {
+			if prev == "" {
+				return "", ErrInvalidString
+			}
+			count, _ := strconv.Atoi(string(char)) // guaranteed by checking "IsDigit"
+			builder.WriteString(strings.Repeat(prev, count))
+			prev = ""
+			continue
+		}
+
+		builder.WriteString(prev)
+		prev = string(char)
+		isEscape = false
+	}
+
+	if isEscape {
+		return "", ErrInvalidString
+	}
+	builder.WriteString(prev)
+
+	return builder.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,15 +16,21 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "abcd", expected: "abcd"},
+		{input: "e 2e", expected: "e  e"},
+		{input: "x1y1", expected: "xy"},
+		{input: "c0", expected: ""},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `\11\22\33`, expected: `122333`},
+		{input: `\\\\`, expected: `\\`},
 	}
 
-	for _, tc := range tests {
-		tc := tc
+	for i := range tests {
+		tc := tests[i]
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
@@ -34,12 +40,34 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
-	for _, tc := range invalidStrings {
-		tc := tc
+	invalidStrings := []string{"3abc", "45", `a\`, "aaa10b", `d2\y`, `\\\`}
+	for i := range invalidStrings {
+		tc := invalidStrings[i]
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+
+func TestUnpackExtraString(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "абв", expected: "абв"},
+		{input: "а1б2в3", expected: "аббввв"},
+		{input: `а\1б2`, expected: "а1бб"},
+		{input: `!@#$%^&*()`, expected: "!@#$%^&*()"},
+		{input: `!@2#3$`, expected: "!@@###$"},
+		{input: `!@\2#\3$`, expected: "!@2#3$"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
 		})
 	}
 }
