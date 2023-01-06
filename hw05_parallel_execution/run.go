8065d88670157eaf5988208b9b29e94649e0dbf3
George Ramaniuk <george.ramaniuk@gmail.com>
2023-01-06 21:09:23 +0300

    Add run

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,77 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var (
+	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+	ErrErrorsCountGor      = errors.New("errors count of goroutines")
+)
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if m < 1 {
+		return ErrErrorsLimitExceeded
+	}
+	if n < 1 {
+		return ErrErrorsCountGor
+	}
+
+	taskCh := make(chan Task)
+	wg := &sync.WaitGroup{}
+	errC := newErrCounter(m)
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go process(taskCh, errC, wg)
+	}
+
+	for _, task := range tasks {
+		if errC.isExceeded() {
+			break
+		}
+		taskCh <- task
+	}
+	close(taskCh)
+
+	wg.Wait()
+
+	if errC.count.Load() > 0 {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
+}
+
+type errCounter struct {
+	limit int32
+	count atomic.Int32
+}
+
+func (e *errCounter) inc() {
+	e.count.Add(1)
+}
+
+func (e *errCounter) isExceeded() bool {
+	return e.count.Load() >= e.limit
+}
+
+func newErrCounter(limit int) *errCounter {
+	return &errCounter{
+		limit: int32(limit),
+		count: atomic.Int32{},
+	}
+}
+
+func process(ch <-chan Task, errC *errCounter, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for task := range ch {
+		if err := task(); err != nil {
+			errC.inc()
+		}
+	}
 }
